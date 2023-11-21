d22eff62bae47093266787ac60a1e270e68caa9d
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-21 18:06:12 -0500

    f

--- nix/cross-image.nix
+++ nix/cross-image.nix
@@ -55,7 +55,7 @@ rec {
   };
 
   cross = 
-  let crossPkgs = (nixpkgs { crossSystem = { config = "aarch64-unknown-linux-gnu"; }; }).pkgsStatic;
+  let crossPkgs = (pkgs { crossSystem = { config = "aarch64-unknown-linux-gnu"; }; }).pkgs;
   in pkgs.dockerTools.buildImage {
       name = "to-build-x86_64";
       tag = "cross";
