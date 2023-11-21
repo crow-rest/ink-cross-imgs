a07faca1f8b756d684a9f46a29df7e1a566da17d
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-21 15:35:34 -0500

    cross?

---
+++ nix/cross-image.nix
@@ -0,0 +1,32 @@
+{ name ? "null", cmd ? ({ cargo }: "${cargo}/bin/cargo"), tagBase ? "latest" }:
+
+let
+  buildImage = arch:
+    { dockerTools, callPackage }:
+    dockerTools.buildImage {
+      inherit name;
+      tag = "${tagBase}-${arch}";
+      config = { Cmd = [ (callPackage cmd { }) ]; };
+    };
+  architectures = [ "x86_64" "aarch64" ];
+  nixpkgs = import <nixpkgs>;
+  crossSystems = map (arch: {
+    inherit arch;
+    pkgs = (nixpkgs {
+      crossSystem = { config = "${arch}-unknown-linux-musl"; };
+    }).pkgsStatic;
+  }) architectures;
+  pkgs = nixpkgs { };
+  lib = pkgs.lib;
+  images = map ({ arch, pkgs }: rec {
+    inherit arch;
+    image = pkgs.callPackage (buildImage arch) { };
+    tag = "${tagBase}-${arch}";
+  }) crossSystems;
+  loadAndPush = builtins.concatStringsSep "\n" (lib.concatMap
+    ({ arch, image, tag }: [
+      "$docker load -i ${image}"
+      # "$docker push ${name}:${tag}"
+    ]) images);
+  imageNames = builtins.concatStringsSep " "
+    (map ({ arch, image, tag }: "${name}:${tag}") images);
