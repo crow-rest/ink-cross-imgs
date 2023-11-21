962c69969423afd99ef0b1aa2ca284e40ec8bbed
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-21 18:00:53 -0500

    fix

--- nix/cross-image.nix
+++ nix/cross-image.nix
@@ -1,45 +1,68 @@
 { pkgs ? import <nixpkgs> { }
 , pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
-, pkgsCross ? import <nixpkgs>
+, pkgsCross ? import <nixpkgs> { crossSystem = "aarch64-unknown-linux-gnu"; }
 }:
 
 rec {
-  # base = pkgs.dockerTools.buildImage {
-  #   name = "to-build";
-  #   tag = "no-push";
-  #   created = "now";
+  base = pkgs.dockerTools.buildImage {
+    name = "to-build-x86_64";
+    tag = "base";
+    created = "now";
 
-  #   architecture = "amd64";
-  #   copyToRoot = pkgs.buildEnv {
-  #     name = "image-root-base";
-  #     paths = [
-  #       pkgsLinux.bash
-  #       pkgsLinux.curl
+    architecture = "amd64";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root-base";
+      paths = [
+        pkgsLinux.bash
+        pkgsLinux.curl
+      ];
+      pathsToLink = [ "/bin" ];
+    };
 
-  #       pkgsLinux.pkg-config
-  #       pkgsLinux.cmake
-  #       pkgsLinux.ninja
-  #     ];
-  #     pathsToLink = [ "/bin" ];
-  #   };
+    config = {
+      CMD = [ "/bin/bash" ];
+      WorkingDir = "/project";
+    };
 
-  #   config = {
-  #     CMD = [ "/bin/bash" ];
-  #     WorkingDir = "/project";
-  #   };
+    diskSize = 10240;
+    buildVMMemorySize = 5120;
+  };
 
-  #   diskSize = 10240;
-  #   buildVMMemorySize = 5120;
-  # };
+  nativeTools = pkgs.dockerTools.buildImage {
+    name = "to-build-x86_64";
+    tag = "nativeTools";
+    created = "now";
+
+    fromImage = base;
+
+    architecture = "amd64";
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root-base";
+      paths = [
+        pkgsLinux.pkg-config
+        pkgsLinux.cmake
+        pkgsLinux.ninja
+      ];
+      pathsToLink = [ "/bin" ];
+    };
+
+    config = {
+      CMD = [ "/bin/bash" ];
+      WorkingDir = "/project";
+    };
+
+    diskSize = 10240;
+    buildVMMemorySize = 5120;
+  };
 
   cross =
     let crossPkgs = pkgsCross.aarch64-unknown-linux-gnu;
     in pkgs.dockerTools.buildImage {
       name = "to-build-x86_64";
-      tag = "no-push";
+      tag = "cross";
       created = "now";
 
-      # fromImage = base;
+      fromImage = nativeTools;
 
       architecture = "amd64";
       copyToRoot = pkgs.buildEnv {
@@ -51,7 +74,7 @@ rec {
           pkgsLinux.cmake
           pkgsLinux.ninja
 
-          crossPkgs.gcc12
+          # crossPkgs.gcc12
         ];
         pathsToLink = [ "/bin" ];
       };
